5185ba57a6691c79ba052350cb3388fe7f6718a2
GoshPro <goshpro@gmail.com>
2015-02-09 03:44:47 +0300

    Чё-то оно как-то коряво работает. =(

--- ResponseTime/Main.cpp
+++ ResponseTime/Main.cpp
@@ -1,26 +1,26 @@
-#include "ResponseTimeData.h"
+#include "SupportResponseTime.h"
 
 #include <iostream>
 
 int main () {
 	srand (static_cast<unsigned int>(time (nullptr)));
-	ResponseTime::Data data;
-	ResponseTime::Timestamp currentTimestamp = 123456;
+	SupportResponseTime::Data data;
+	time_t currentTimestamp = 123456;
 	std::cout << "Test\n" << 0123 << "\n\n";
-	ResponseTime::NumberOfTickets results[ResponseTime::NUMBER_OF_COHORTS][ResponseTime::MIN_HISTORY_LENGTH];
-	for(size_t hour = 0; hour < ResponseTime::MIN_HISTORY_LENGTH; ++hour) {
-		for(size_t cohortIndex = 0; cohortIndex < ResponseTime::NUMBER_OF_COHORTS; ++cohortIndex) {
+	SupportResponseTime::NumberOfTickets results[SupportResponseTime::NUMBER_OF_COHORTS][SupportResponseTime::MIN_HISTORY_LENGTH];
+	for(size_t hour = 0; hour < SupportResponseTime::MIN_HISTORY_LENGTH; ++hour) {
+		for(size_t cohortIndex = 0; cohortIndex < SupportResponseTime::NUMBER_OF_COHORTS; ++cohortIndex) {
 			results[cohortIndex][hour] = 100 * (cohortIndex + 1);
 		}
 	}
 	data.setCohortsResults (results, currentTimestamp);
-	for(size_t i = 0; i < 100; ++i) {
-		currentTimestamp += ResponseTime::SECONDS_IN_HOUR;
-		ResponseTime::NumberOfTickets numberOfTickets[3] = { 100 + rand () % 100, 200 + rand () % 100, 300 + rand () % 100 };
+	for(size_t i = 0; i < 10; ++i) {
+		currentTimestamp += SupportResponseTime::SECONDS_IN_HOUR;
+		SupportResponseTime::NumberOfTickets numberOfTickets[3] = { 100 + rand () % 100, 200 + rand () % 100, 300 + rand () % 100 };
 		data.switchHour (numberOfTickets, currentTimestamp);
 	}
-	data.print (-20, ResponseTime::PROGNOSIS_DEPTH - 1);
-	ResponseTime::NumberOfTickets ticketsQueue = 100000;
+	data.print (0, SupportResponseTime::PROGNOSIS_DEPTH - 1);
+	SupportResponseTime::NumberOfTickets ticketsQueue = 100000;
 	data.calculateResponseTime (ticketsQueue, currentTimestamp);
 	std::cout << "\nQueue: " << ticketsQueue << " ticket(s)\nResponse time: " << data.getResponseTime () << " hour(s)\n\n";
 	int a = 0, b = 0;
